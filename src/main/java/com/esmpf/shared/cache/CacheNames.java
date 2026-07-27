172293c01027fcf775c21bc45dcb7a8c076c2f1b
alimbetov <51784151+alimbetov@users.noreply.github.com>
2026-07-27 17:52:30 +0500

    Add shared cache names

---
+++ src/main/java/com/esmpf/shared/cache/CacheNames.java
@@ -0,0 +1,17 @@
+package com.esmpf.shared.cache;
+
+public final class CacheNames {
+    public static final String CATALOG_EQUIPMENT_TYPE = "catalogEquipmentType";
+    public static final String CATALOG_JOB_TYPE = "catalogJobType";
+    public static final String CATALOG_CHECKLIST_TEMPLATE = "catalogChecklistTemplate";
+    public static final String CATALOG_MAINTENANCE_TEMPLATE = "catalogMaintenanceTemplate";
+    public static final String CATALOG_UNIT = "catalogUnit";
+    public static final String CUSTOMER_REFERENCE = "customerReference";
+    public static final String SERVICE_LOCATION_REFERENCE = "serviceLocationReference";
+    public static final String EQUIPMENT_REFERENCE = "equipmentReference";
+    public static final String IDENTITY_USER_REFERENCE = "identityUserReference";
+    public static final String CURRENT_BUSINESS = "currentBusiness";
+
+    private CacheNames() {
+    }
+}
